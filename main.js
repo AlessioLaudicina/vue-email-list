02ce7e808008bbc07549e33beb24277daf9f6f41
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-03-23 15:17:19 +0100

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+/* generare 10 indirizzi email e stamparli in pagina all’interno di una lista. */
+
+
+
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
